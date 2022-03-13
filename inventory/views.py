55e1d3a1b12af8f58c2397d585625e17f29f16a4
Matthew <matthewrr@users.noreply.github.com>
2022-03-12 17:33:18 -0800

    Import qrcode and add views.py function to generate QR code image

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+import qrcode
 
 #move to class-based views
 
@@ -14,7 +15,6 @@ def add_item(request):
     fixtures = Fixture.objects.all()
     containers = Container.objects.all()
     articles = Article.objects.all()
-    
     context = { 
         'models': {
             'Buildings': buildings,
@@ -24,4 +24,13 @@ def add_item(request):
             'Articles': articles
     }}
 
+    data = buildings
+    qr_code = create_qr(data)
+
     return render(request, 'inventory/content.html', context)
+
+def create_qr(data):
+    img = qrcode.make(data)
+    file_name = 'MyQRCode2.png'
+    img.save(file_name)
+    return
